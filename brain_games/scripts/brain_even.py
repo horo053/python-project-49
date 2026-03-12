2b9809780370be1899853f5cff19f3af94693315
horo053 <yulfi555@gmail.com>
2026-03-12 20:07:29 +0300

    изменение игры: Проверка на чётность

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -2,34 +2,37 @@ from random import randint
 import prompt
 
 
-def parity_check():
+def even_check():
     print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
     print('Answer "yes" if the number is even, otherwise answer "no".')
-    k = 0
-    for i in range(3):
+
+    count_correct_answers = 0
+
+    for _ in range(3):
         number = randint(1, 100)
         print(f'Question: {number}')
-        resp = input('Your answer: ')
-        if number % 10 in (1, 3, 5, 7, 9) and resp == 'no':
+
+        is_even = number % 2 == 0
+        correct_answer = 'yes' if is_even else 'no'
+
+        user_answer = input('Your answer: ')
+
+        if user_answer == correct_answer:
             print('Correct!')
-        elif number % 10 in (2, 4, 6, 8, 0) and resp == 'yes':
-            print('Correct!')
+            count_correct_answers += 1
         else:
-            if resp == 'no':
-                print('\'no\' is wrong answer ;(. Correct answer was \'yes\'.')
-            else:
-                print('\'yes\' is wrong answer ;(. Correct answer was \'no\'.')
+            print(f"'{user_answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
             print(f'Let\'s try again, {name}!')
             break
-        k += 1
-    if k == 3:
+
+    if count_correct_answers == 3:
         print(f'Congratulations, {name}!')
 
 
 def main():
-    parity_check()
+    even_check()
 
 
 if __name__ == "__main__":
