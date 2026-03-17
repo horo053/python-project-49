5d8c3e4052e1466e92148c039c45a9d7ebe5dcc8
horo053 <yulfi555@gmail.com>
2026-03-17 19:56:06 +0300

    доработка оформления

--- brain_games/cycle_of_rounds.py
+++ brain_games/cycle_of_rounds.py
@@ -1,7 +1,8 @@
 def cycle_of_rounds(func_game, name):
     count_correct_answers = 0
+    count_round = 3
 
-    for _ in range(3):
+    for _ in range(count_round):
         user_answer, correct_answer = func_game()
 
         if int(user_answer) == int(correct_answer):
@@ -11,7 +12,7 @@ def cycle_of_rounds(func_game, name):
             print(f"'{user_answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
             break
 
-    if count_correct_answers == 3:
+    if count_correct_answers == count_round:
         print(f'Congratulations, {name}!')
     else:
         print(f'Let\'s try again, {name}!')
